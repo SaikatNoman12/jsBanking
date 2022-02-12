83abfc4f520e14262a18f836a553813c747492a6
SaikatNoman12 <noommaa240@gmail.com>
2022-02-12 13:17:03 +0600

    add new js function

--- js/banking.js
+++ js/banking.js
@@ -8,3 +8,22 @@ function inputValue(inputId){
     return allInputNum; 
 };
 
+// this function use deposit and withdraw amount id callded  
+function totalMoney(blanceId){
+    const totalBlance = document.getElementById(blanceId);
+    const totalBlanceText = totalBlance.innerText;
+    const totalBlanceAmount = parseFloat(totalBlanceText);
+    return totalBlanceAmount;
+};
+
+// this function use only deposit and withdraw input total calculation
+function showValue(windowId , inputAmount){
+    const showWindowText = document.getElementById(windowId);
+    const windowConvertNumber = totalMoney(windowId)
+
+    // total blance
+    const totalBlance = windowConvertNumber + inputAmount 
+    showWindowText.innerText = totalBlance;
+
+};
+
